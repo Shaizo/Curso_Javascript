7c1971bfed66ba554182cb8dfe7e53f25fc66230
Aitor PC Casa <aitorlevi@gmail.com>
2018-09-16 20:15:19 +0200

    commit hasta el fichero 26

---
+++ js/25-arrays.js
@@ -0,0 +1,17 @@
+'use strict'
+
+//Arrays, Arreglos, Matrices...
+
+var nombre = "Aitor";
+var nombres = ["Jose", "Carlos", "Koke"];
+
+var lenguajes = new Array("PHP", "JS", "Go", "Java");
+
+
+// for (var i = 0; i < lenguajes.length; i++) {
+// 	console.log(lenguajes[i]);
+// }
+
+lenguajes.forEach((elemento)=>{
+	console.log(elemento);
+});
---
+++ js/26-arrays-multi.js
@@ -0,0 +1,19 @@
+'use strict'
+
+var categorias = ['Accion', 'Terror', 'Comedia'];
+var peliculas = ['La verdad duele', 'Antman', 'Salchicha'];
+
+var cine = [categorias, peliculas]; 
+console.log(peliculas);
+//Añadir ultimo
+peliculas.push("Batman");
+console.log(peliculas);
+
+//Borrar ultimo
+peliculas.pop();
+console.log(peliculas);
+
+//Pasar de array a string
+var texto = peliculas.join();
+
+console.log(texto);
